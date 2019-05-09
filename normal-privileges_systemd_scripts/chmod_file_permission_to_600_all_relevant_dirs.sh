f4723726bd3ba4c5490473308c398ad2f4a70f3d
Florian Begusch <florian.begusch@gmail.com>
2019-05-09 15:47:54 +0200

    Generalize chmod_file_permission_to_600 script

---
+++ normal-privileges_systemd_scripts/chmod_file_permission_to_600_all_relevant_dirs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+user=`cut -d : -f 1 /etc/passwd | grep flo`
+user_dir=/home/$user
+
+find $user_dir/Pictures/ -type f -exec chmod 600 {} \;
+find $user_dir/Pictures/ -type d -exec chmod 700 {} \;
+
+find $user_dir/.config/Signal -type f -exec chmod 600 {} \;
+find $user_dir/.config/Signal -type d -exec chmod 700 {} \;
+
+
+find $user_dir/Documents/wir -type f -exec chmod 600 {} \;
+find $user_dir/Documents/wir -type d -exec chmod 700 {} \;
+
