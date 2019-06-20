e648420011aba94c820e5449a1efdf3d1e4bccb6
Florian Begusch <florian.begusch@gmail.com>
2019-06-20 10:55:10 +0200

    Add standard notes directories to chmod_file_permission_to_600_all_relevant_dirs.sh

--- normal-privileges_systemd_scripts/chmod_file_permission_to_600_all_relevant_dirs.sh
+++ normal-privileges_systemd_scripts/chmod_file_permission_to_600_all_relevant_dirs.sh
@@ -7,6 +7,8 @@ script_file=$user_dir/Documents/scripts/set___user_rw_noone_else.sh
 $script_file $user_dir/Pictures/
 $script_file $user_dir/Documents/misc
 $script_file $user_dir/.config/Signal
+$script_file $user_dir/.config/'Standard Notes'
+$script_file $user_dir/'Standard Notes Backups'
 $script_file $user_dir/.config/fish
 $script_file $user_dir/.local/share/fish
 $script_file $user_dir/Documents/wir
