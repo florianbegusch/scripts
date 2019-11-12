eddd015aa428e73a124c235da5b6e3914c295e26
Florian Begusch <florian.begusch@gmail.com>
2019-11-12 07:07:33 +0100

    Fix chmod_file_permission_to_600_all_relevant_dirs

--- normal-privileges_systemd_scripts/chmod_file_permission_to_600_all_relevant_dirs.sh
+++ normal-privileges_systemd_scripts/chmod_file_permission_to_600_all_relevant_dirs.sh
@@ -2,13 +2,13 @@
 
 script_file=~/Documents/scripts/set___user_rw_noone_else.sh
 
-$script_file $user_dir/Pictures/
-$script_file $user_dir/Documents/misc
-$script_file $user_dir/.config/Signal
-$script_file $user_dir/.config/'Standard Notes'
-$script_file $user_dir/'Standard Notes Backups'
-$script_file $user_dir/.config/fish
-$script_file $user_dir/.local/share/fish
-$script_file $user_dir/Documents/wir
-$script_file $user_dir/ISOs
+$script_file ~/Pictures/
+$script_file ~/Documents/misc
+$script_file ~/.config/Signal
+$script_file ~/.config/'Standard Notes'
+$script_file ~/'Standard Notes Backups'
+$script_file ~/.config/fish
+$script_file ~/.local/share/fish
+$script_file ~/Documents/wir
+$script_file ~/ISOs
 
