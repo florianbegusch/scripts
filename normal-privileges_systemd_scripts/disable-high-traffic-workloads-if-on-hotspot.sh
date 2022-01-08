da5cf4a473ae90f7d33ee687c00360142a9c3afd
Florian Begusch <florian.begusch@gmail.com>
2022-01-08 10:58:05 +0100

    cleanup

--- normal-privileges_systemd_scripts/disable-high-traffic-workloads-if-on-hotspot.sh
+++ normal-privileges_systemd_scripts/disable-high-traffic-workloads-if-on-hotspot.sh
@@ -1,7 +1,20 @@
 #!/usr/bin/env bash
 
 services=()
-services+=('***REMOVED***_and_***REMOVED***.service')
+services+=('bibi_and_benjamin.service')
+services+=('container-image-update.service')
+services+=('infosec_pkgs_update.service')
+
+if [ "$(iwgetid -r)" = iPhone ] || [ "$1" = -f ]; then
+  for service in "${services[@]}"; do
+    set -x
+    systemctl stop --user "$service"
+    set +x
+  done
+fi
+
+unset services
+
 services+=('container-image-update.service')
 services+=('infosec_pkgs_update.service')
 
